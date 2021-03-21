09c18b3feb5cb719d632792d50b910a3d6bf6e82
GoldenkovVitali <vitalik.resscuer@mail.ru>
2021-03-21 10:15:54 +0300

    fix: change CRLF to LF

--- src/app/shared/directives/on-click-marker/on-click-marker.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { OnClickMarkerDirective } from './on-click-marker.directive';
-
-describe('OnClickMarkerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OnClickMarkerDirective();
-
-    expect(directive).toBeTruthy();
-  });
-});
